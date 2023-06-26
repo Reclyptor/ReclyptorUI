5e3b874f392ee4854bc983eb1e2161c8e087160d
Emilio Torres Jr <emiliotorres3193@gmail.com>
2023-06-26 03:25:30 -0500

    Updated Checkbox Component

--- src/component/Input/Checkbox/Checkbox.tsx
+++ src/component/Input/Checkbox/Checkbox.tsx
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
-import { Check as CheckIcon, Minus as IndeterminateIcon } from 'lucide-react';
-import { CheckboxProps } from './Checkbox.types';
-import { color } from '../../../util/colors';
-import useHAF from '../../../hook/useHAF';
+import React, { ChangeEvent, FormEvent, forwardRef, useState } from "react";
 import clsx from 'clsx';
+import useHAF from '../../../hook/useHAF';
+import useProps from "../../../hook/useProps";
+import { Check as CheckIcon, Minus as IndeterminateIcon } from 'lucide-react';
+import { CheckboxProps, StandardProps } from "./Checkbox.types";
+import { color } from '../../../util/colors';
 import '../../../tailwind.css';
 
-const Checkbox = (props: CheckboxProps) => {
-  const { ref, hover, active } = useHAF<HTMLButtonElement>();
-  const [checked, setChecked] = useState<boolean>(!!props.checked);
+const Checkbox = forwardRef<HTMLInputElement, CheckboxProps & StandardProps>((_props, _ref) => {
+  const { props, rest } = useProps<CheckboxProps, StandardProps>(_props, ["indeterminate", "size", "className", "style"]);
+  const { ref, hover, active } = useHAF<HTMLInputElement>();
+  const [checked, setChecked] = useState<boolean>(!!rest.checked);
 
   const onClick = (): void => {
     setChecked(!checked);
-    props.onChange?.(!checked);
   };
 
   const fill = (): string => {
@@ -26,17 +27,16 @@ const Checkbox = (props: CheckboxProps) => {
   };
 
   return (
-    <button ref={ ref } onClick={ onClick } className={ props.className }>
-      <div className={ clsx("w-4 h-4 rounded-sm bg-foreground-mute border-2 border-primary-mute", { "bg-foreground border-primary": hover, "bg-foreground-accent border-primary-accent": active }) }>
-        { (checked || hover) && (
-          props.indeterminate ?
-            <IndeterminateIcon size={ 12 } strokeWidth={ 4 } color={ color(fill()) } className={ clsx("min-w-3", { "opacity-40": !checked }) } /> :
-            <CheckIcon size={ 12 } strokeWidth={ 4 } color={ color(fill()) } className={ clsx("min-w-3", { "opacity-40": !checked }) } />
-          )
-        }
-      </div>
-    </button>
+    <div ref={ ref } onClick={ onClick } className={ clsx("flex items-center justify-center rounded-sm bg-foreground-mute border-2 border-primary-mute", { "bg-foreground border-primary": hover, "bg-foreground-accent border-primary-accent": active, "cursor-pointer": !rest.disabled, "w-4 h-4": props.size === "sm", "w-5 h-5": !props.size || props.size === "md", "w-6 h-6": props.size === "lg" }, props.className) } style={ props.style }>
+      <input { ...rest } ref={ _ref } type="checkbox" className="hidden" checked={ checked } />
+      {
+        (checked || hover) && (props.indeterminate ?
+          <IndeterminateIcon size={ clsx({ [12]: props.size === "sm", [14]: !props.size || props.size === "md", [18]: props.size === "lg" }) } strokeWidth={ 4 } color={ color(fill()) } className={ clsx("min-w-3", { "opacity-40": !checked }) } /> :
+          <CheckIcon size={ clsx({ [12]: props.size === "sm", [14]: !props.size || props.size === "md", [18]: props.size === "lg" }) } strokeWidth={ 4 } color={ color(fill()) } className={ clsx("min-w-3", { "opacity-40": !checked }) } />
+        )
+      }
+    </div>
   );
-};
+});
 
 export default Checkbox;
--- src/component/Input/Checkbox/Checkbox.types.ts
+++ src/component/Input/Checkbox/Checkbox.types.ts
@@ -1,6 +1,10 @@
-export interface CheckboxProps {
-    checked?: boolean
-    indeterminate?: boolean
-    onChange?(_: boolean): void
-    className?: string
-}
+import { CSSProperties, DetailedHTMLProps, InputHTMLAttributes } from "react";
+
+export type CheckboxProps = {
+  indeterminate?: boolean;
+  size?: 'sm' | 'md' | 'lg'; // Default 'md'
+  className?: string;
+  style?: CSSProperties;
+};
+
+export type StandardProps = Omit<DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, keyof CheckboxProps>;
