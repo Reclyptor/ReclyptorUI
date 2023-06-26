705063d16c3b562b6dc9ccafd4de35b51f098b93
Emilio Torres Jr <emiliotorres3193@gmail.com>
2023-06-26 04:01:09 -0500

    Updated Toggle Component

--- src/component/Input/Toggle/Toggle.tsx
+++ src/component/Input/Toggle/Toggle.tsx
@@ -14,12 +14,12 @@ const Toggle = (props: ToggleProps) => {
   };
 
   return (
-    <div className={ props.className }>
-      <div className="flex items-center w-8 h-2 rounded-lg bg-foreground">
-        <button onClick={ onClick } className={ clsx("flex items-center justify-center w-4 h-4 rounded-full bg-foreground-mute cursor-pointer transition ease-in-out duration-300 hover:outline hover:outline-none hover:outline-2 hover:outline-primary", { "translate-x-full bg-primary": toggled }) }>
+    <div className={ props.className } style={ props.style }>
+      <div className={ clsx("flex items-center rounded-lg bg-foreground", { "w-8 h-2": props.size === "sm", "w-10 h-[10px]": !props.size || props.size === "md", "w-12 h-[12px]": props.size === "lg" }) }>
+        <button onClick={ onClick } className={ clsx("flex items-center justify-center rounded-full bg-foreground-mute cursor-pointer transition ease-in-out duration-300 hover:border-2 hover:border-primary", { "translate-x-full bg-primary": toggled, "w-[18px] h-[18px]": props.size === "sm", "w-[21px] h-[21px]": !props.size || props.size === "md", "w-6 h-6": props.size === "lg" }) }>
           { toggled ?
-            <ToggledIcon size={ 12 } strokeWidth={ 4 } color={ color('foreground-mute') } className="min-w-3" /> :
-            <NotToggledIcon size={ 12 } strokeWidth={ 4 } color={ color('primary') } className="min-w-3" />
+            <ToggledIcon size={ clsx({ [12]: props.size === "sm", [15]: !props.size || props.size === "md", [16]: props.size === "lg" }) } strokeWidth={ 4 } color={ color('foreground-mute') } className={ clsx({ "min-w-[18px]": props.size === "sm", "min-w-[21px]": !props.size || props.size === "md", "min-w-6": props.size === "lg" }) } /> :
+            <NotToggledIcon size={ clsx({ [12]: props.size === "sm", [15]: !props.size || props.size === "md", [16]: props.size === "lg" }) } strokeWidth={ 4 } color={ color('primary') } className={ clsx({ "min-w-[18px]": props.size === "sm", "min-w-[21px]": !props.size || props.size === "md", "min-w-6": props.size === "lg" }) } />
           }
         </button>
       </div>
--- src/component/Input/Toggle/Toggle.types.ts
+++ src/component/Input/Toggle/Toggle.types.ts
@@ -1,5 +1,9 @@
+import { CSSProperties } from "react";
+
 export interface ToggleProps {
-    toggled?: boolean
-    onChange?(_: boolean): void
-    className?: string
+    toggled?: boolean;
+    onChange?(_: boolean): void;
+    size?: 'sm' | 'md' | 'lg'; // Default 'md'
+    className?: string;
+    style?: CSSProperties;
 }
