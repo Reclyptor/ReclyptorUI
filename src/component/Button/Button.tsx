417050c7d744927d3e0e01759be4b68693ac1555
Emilio Torres Jr <emilio@reclyptor.com>
2023-10-18 01:30:17 -0500

    Added Tailwind Merge Library

--- src/component/Button/Button.tsx
+++ src/component/Button/Button.tsx
@@ -4,12 +4,13 @@ import { clazz } from "./Button.styles";
 import useProps from "../../hook/useProps";
 import clsx from "clsx";
 import "../../tailwind.css";
+import { twMerge } from "tailwind-merge";
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps & StandardProps>((_props, _ref) => {
   const { props, rest } = useProps<ButtonProps, StandardProps>(_props, ["solid", "size", "variant"]);
 
   return (
-    <button { ...rest } ref={ _ref } className={ clsx(clazz(props), rest.className) }>
+    <button { ...rest } ref={ _ref } className={ twMerge(clsx(clazz(props), rest.className)) }>
       { React.isValidElement(rest.children) ? rest.children : <div className="flex items-center justify-center w-full h-full">{ rest.children }</div> }
     </button>
   );
--- src/component/Card/Card.tsx
+++ src/component/Card/Card.tsx
@@ -4,12 +4,13 @@ import { clazz } from "./Card.styles";
 import useProps from "../../hook/useProps";
 import clsx from "clsx";
 import "../../tailwind.css";
+import { twMerge } from "tailwind-merge";
 
 const Card = forwardRef<HTMLDivElement, CardProps & StandardProps>((_props, _ref) => {
   const { props, rest } = useProps<CardProps, StandardProps>(_props, ["flip", "solid", "front", "back", "variant"]);
 
   return (
-    <div { ...rest } ref={ _ref } className={ clsx(clazz(props), rest.className) }>
+    <div { ...rest } ref={ _ref } className={ twMerge(clsx(clazz(props), rest.className)) }>
       { rest.children ?
         <div className="flex items-center justify-center w-full h-full bg-[inherit] rounded-[inherit] [backface-visibility:hidden]">{ rest.children }</div> :
         <>
